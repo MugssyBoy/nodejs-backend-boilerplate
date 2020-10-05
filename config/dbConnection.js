6c93f01e402bd2e15fd292082fa6ec4669bab267
alex <43148847+MugssyBoy@users.noreply.github.com>
2020-10-05 09:01:31 +0800

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,11 +4,28 @@ const options = {
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    queueLimit: 0,
+    acquireTimeout: 10000,
+    waitForConnections: true,
+    multipleStatements: true,
 };
 
-const connection = mysql.createConnection(options);
+const pool = mysql.createPool(options);
 
-connection.connect(err => err ? console.error('db error connecting: ' + err.stack) : console.log('connected as id ' + connection.threadId));
+pool.getConnection((err, connection) => {
+ if (err) {
+     if(err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error('Database connection was closed')
+     }
+     if(err.code === "ER_CON_COUNT_ERROR") {
+      console.error('Database has too many connections.')
+     }
+     if(err.code === "ECONREFUSED") {
+      console.error('Database connection was released.')
+     }
+ }
+        return connection
+})
 
-module.exports = connection;
+module.exports = pool;
